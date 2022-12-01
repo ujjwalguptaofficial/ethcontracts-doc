f17d57aaa4f8e7a76a848f2d43035a6ff1ba5fcc
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2022-12-01 07:57:42 +0530

    add keywords

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      metadata: [{ name: 'keywords', content: 'ethcontracts.js, smart contracts, javascript, implementation, ethereum,' }],
       navbar: {
         title: 'EthContracts',
         // logo: {
@@ -77,51 +78,51 @@ const config = {
           },
         ],
       },
-        // footer: {
-        //   style: 'dark',
-        //   links: [
-        //     {
-        //       title: 'Docs',
-        //       items: [
-        //         {
-        //           label: 'Tutorial',
-        //           to: '/docs/intro',
-        //         },
-        //       ],
-        //     },
-        //     {
-        //       title: 'Community',
-        //       items: [
-        //         {
-        //           label: 'Stack Overflow',
-        //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-        //         },
-        //         {
-        //           label: 'Discord',
-        //           href: 'https://discordapp.com/invite/docusaurus',
-        //         },
-        //         {
-        //           label: 'Twitter',
-        //           href: 'https://twitter.com/docusaurus',
-        //         },
-        //       ],
-        //     },
-        //     {
-        //       title: 'More',
-        //       items: [
-        //         {
-        //           label: 'Blog',
-        //           to: '/blog',
-        //         },
-        //         {
-        //           label: 'GitHub',
-        //           href: 'https://github.com/facebook/docusaurus',
-        //         },
-        //       ],
-        //     },
-        //   ],
-        //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-        // },
+      // footer: {
+      //   style: 'dark',
+      //   links: [
+      //     {
+      //       title: 'Docs',
+      //       items: [
+      //         {
+      //           label: 'Tutorial',
+      //           to: '/docs/intro',
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       title: 'Community',
+      //       items: [
+      //         {
+      //           label: 'Stack Overflow',
+      //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+      //         },
+      //         {
+      //           label: 'Discord',
+      //           href: 'https://discordapp.com/invite/docusaurus',
+      //         },
+      //         {
+      //           label: 'Twitter',
+      //           href: 'https://twitter.com/docusaurus',
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       title: 'More',
+      //       items: [
+      //         {
+      //           label: 'Blog',
+      //           to: '/blog',
+      //         },
+      //         {
+      //           label: 'GitHub',
+      //           href: 'https://github.com/facebook/docusaurus',
+      //         },
+      //       ],
+      //     },
+      //   ],
+      //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      // },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
